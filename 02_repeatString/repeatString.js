028015e13c8c9c4b4d0ad16105c15ae736cb5adb
jtaylorwalk <jtaylorwalk@gmail.com>
2024-09-13 15:37:29 -0600

    create repeatString function to repeat string a, b number of times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
 
+const repeatString = function(a,b) {
+    if (b < 0){
+        return "ERROR";
+    }
+
+    else {
+    let stResult = "";
+    for (let i=b; i>0; i--) {
+        stResult = stResult + a;
+    }
+    return stResult;
+}
 };
 
 // Do not edit below this line
