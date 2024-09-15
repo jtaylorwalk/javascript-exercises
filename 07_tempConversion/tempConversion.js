d5001593c0444655e2e4b35721c04190c9194b15
jtaylorwalk <jtaylorwalk@gmail.com>
2024-09-14 22:30:13 -0600

    create converToCelsius and convertToFarenheit

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farInput) {
+let celTen = ((farInput - 32) * 5 / 9 * 10)
+return (Math.round(celTen)/10)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celInput) {
+let farTen = (((celInput * 9 / 5) + 32) * 10)
+return (Math.round(farTen)/10)
 };
+
+
+
 
 // Do not edit below this line
 module.exports = {
