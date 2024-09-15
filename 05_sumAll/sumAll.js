282db922cafe6440748f55c19a3a5848c5a9390b
jtaylorwalk <jtaylorwalk@gmail.com>
2024-09-14 21:18:21 -0600

    create sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
+const sumAll = function(x,y) {
+if (x > 0 && y > 0 && Number.isInteger(x) && Number.isInteger(y) ) {
+    if (x<y) {
+       return ((y-x+1) * (x+y) / 2)
+    }
+    else {
+       return ((x-y+1)*(x+y)/2 )
+    }
+}
+else {
+    return ("ERROR")
+}
 
 };
+
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
