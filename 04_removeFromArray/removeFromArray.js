712703e580591e646fb2c902c7a68feb4dd876b9
jtaylorwalk <jtaylorwalk@gmail.com>
2024-09-14 19:36:42 -0600

    create removeFromArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,43 @@
-const removeFromArray = function() {
+const removeFromArray = function(fullArray, ...removeList) {
+let rLoc = 0
+let fLoc = 0
+const newList = []
+
+    while (fLoc < fullArray.length) {
+                if (rLoc < removeList.length){
+                    
+                        if (fullArray[fLoc] === removeList[rLoc]){
+                        fLoc ++
+                        rLoc = 0
+                        
+                        }
+                        else {
+                            rLoc ++
+                            
+                        }
+                    }
+                else if (rLoc === removeList.length) {
+                        newList.push(fullArray[fLoc])
+                            fLoc ++
+                            rLoc = 0
+                        }
+    }
+if (fLoc === fullArray.length) {
+console.log (newList)
+return newList
+}   
+
+        
+    
+   
+
+        
 };
+// Check each element from fullArray against the first item in
+// removeList. If the element is not in removeList, copy it to
+// returnArray. Loop through all items in removeList. Return  
+//
+//
 
 // Do not edit below this line
 module.exports = removeFromArray;
